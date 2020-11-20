b9efba4d16e32c2f1d2b50124b8d412f0cf975bc
pndbear <tatu.ioana@gmail.com>
2020-11-20 12:33:59 +0100

    Feat: update webpack config to support css modules
    Install style-loader and css-loader packages

--- src/Welcome/Welcome.js
+++ src/Welcome/Welcome.js
@@ -1,14 +1,17 @@
+// local styles
+import css from "./welcome.module.css";
+
 import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
-import Registration from "./registration.js";
-import Login from "./login.js";
+import Registration from "../registration.js";
+import Login from "../login.js";
 
 export default function Welcome() {
     console.log("welcome.js");
 
     return (
-        <div className="welcome-wrapper" autoComplete="off">
+        <div className={css.WelcomeWrapper} autoComplete="off">
             <HashRouter>
                 <div className="welcome-intro">
                     <h1>
