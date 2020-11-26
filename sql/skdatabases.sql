889c655e70776c955310046906655a7f631453ef
Ida Loggert <ida.loggert@gmail.com>
2020-11-26 17:33:49 +0100

    fix: remove comment

--- sql/skdatabases.sql
+++ sql/skdatabases.sql
@@ -53,22 +53,12 @@
     id SERIAL PRIMARY KEY,
     group_id INT NOT NULL REFERENCES groups(id),
     user_id INT REFERENCES users(id) NOT NULL,
-    task_id INT NOT NULL ,
+    task_id INT NOT NULL,
     week INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     done BOOLEAN DEFAULT false
 );
 
-/* Old version before removal of assignment_task_id_fkey to tasks*/
-/* CREATE TABLE assignment(
-    id SERIAL PRIMARY KEY,
-    group_id INT NOT NULL REFERENCES groups(id),
-    user_id INT REFERENCES users(id) NOT NULL,
-    task_id INT NOT NULL REFERENCES tasks(id),
-    week INT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    done BOOLEAN DEFAULT false
-); */
 
 
 /* nice to have if time:
